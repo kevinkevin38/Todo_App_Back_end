a2f90128c7f73151c80731cc123e7e00443430b6
Kevin Franco <kevin.franco@pcsedu.org>
2017-10-21 18:07:16 +0000

    dynamic route

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,8 +2,6 @@ class TodoController < ApplicationController
     def index 
     end
     def show
-        @todo_description = "Make the curriculum"
-        @todo_pomodoro_estimate = 4
     end
 end
 
